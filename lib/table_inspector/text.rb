579980a9bac95d837c6df5e20b7eabdd27a5ffc8
ian <ianlynxk@gmail.com>
2023-03-02 23:27:48 +0800

    Add colorize method to Text module

--- lib/table_inspector/text.rb
+++ lib/table_inspector/text.rb
@@ -10,5 +10,25 @@ module TableInspector
     def break_line
       puts "\n"
     end
+    
+    def red(text)
+      "\e[31m#{text}\e[0m"
+    end
+
+    def green(text)
+      "\e[32m#{text}\e[0m"
+    end
+    
+    def yellow(text)
+      "\e[33m#{text}\e[0m"
+    end
+    
+    def blue(text)
+      "\e[34m#{text}\e[0m"
+    end
+
+    def cyan(text)
+      "\e[36m#{text}\e[0m"
+    end
   end
 end
