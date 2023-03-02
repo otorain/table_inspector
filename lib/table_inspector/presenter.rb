79cdf261d439fe00da9024ea6bf33b0a6c98d586
ian <ianlynxk@gmail.com>
2023-03-02 23:31:35 +0800

    Add ability to print colorful data to Presenter

--- lib/table_inspector/presenter.rb
+++ lib/table_inspector/presenter.rb
@@ -3,13 +3,37 @@ module TableInspector
   class Presenter
     attr_reader :klass, :sql_type
 
-    def initialize(klass, sql_type:)
+    def initialize(klass, sql_type:, colorize: false)
       @klass = klass
       @sql_type = sql_type
+      @colorize = colorize
+    end
+
+    def extract_meta_with_highlight(column)
+      column_data = column.as_json.merge(column.sql_type_metadata.as_json)
+
+      # Colorize text but except "comment" field
+      column_data.each do |k, v|
+        if k != "comment"
+          column_data[k] = colorize(v)
+        else
+          column_data[k]
+        end
+      end
+
+      column_data.slice(*ordered_keys)
+    end
+
+    def extract_meta_without_highlight(column)
+      column.as_json.merge(column.sql_type_metadata.as_json).slice(*ordered_keys)
     end
 
     def extract_meta(column)
-      column.as_json.merge(column.sql_type_metadata.as_json).slice(*ordered_keys)
+      if @colorize
+        extract_meta_with_highlight(column)
+      else
+        extract_meta_without_highlight(column)
+      end
     end
 
     def header
@@ -22,6 +46,23 @@ module TableInspector
     def ordered_keys
       %w[name type limit null default precision scale comment].tap do |keys|
         keys << "sql_type" if sql_type
+      end
+    end
+
+    def colorize(value)
+      case value
+      when TrueClass, DateTime, 'datetime'
+        Text.green(value)
+      when FalseClass
+        Text.red(value)
+      when Numeric, 'integer', 'decimal'
+        Text.blue(value)
+      when 'boolean'
+        Text.cyan(value)
+      when String
+        Text.yellow(value)
+      else
+        value
       end
     end
   end
